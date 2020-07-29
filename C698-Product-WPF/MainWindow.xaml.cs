f7264a906962ddc4b4c4ea099864e5199db9829c
Unknown <ajbeeman@kfsoftwaresolutions.com>
2020-07-28 20:23:11 -0500

    WIP: Styles and Buttons

--- C698-Product-WPF/MainWindow.xaml.cs
+++ C698-Product-WPF/MainWindow.xaml.cs
@@ -17,13 +17,48 @@ namespace C698_Product_WPF
       _partRepository = partRepository;
       _productRepository = productRepository;
       InitializeComponent();
-      GetDataAsync();
+      GetData();
     }
 
-    private async Task GetDataAsync()
+    private async Task GetData()
     {
       PartsGrid.ItemsSource = await _partRepository.GetAll();
       ProductsGrid.ItemsSource = await _productRepository.GetAll();
+    }
+
+    private void Exit_Click(object s, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Parts_Add_Click(object s, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Parts_Modify_Click(object s, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Parts_Delete_Click(object s, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Products_Add_Click(object s, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Products_Modify_Click(object s, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Products_Delete_Click(object s, RoutedEventArgs e)
+    {
+
     }
   }
 }
