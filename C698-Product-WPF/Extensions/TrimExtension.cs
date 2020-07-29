e9b7e5667a1a0cd9b9cb64c24dec5a3865331e51
Unknown <ajbeeman@kfsoftwaresolutions.com>
2020-07-29 06:14:00 -0500

    WIP: Search Products/Parts & Exit App Functionality

---
+++ C698-Product-WPF/Extensions/TrimExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C698_Product_WPF.Extensions
+{
+  public static class TrimExtension
+  {
+    public static string TrimFix(this string rawString)
+    {
+      if (!string.IsNullOrWhiteSpace(rawString))
+      {
+        return rawString.Trim();
+      }
+      return null;
+    }
+  }
+}
--- C698-Product-WPF/MainWindow.xaml.cs
+++ C698-Product-WPF/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using C698_Product_WPF.EntityModels;
+using C698_Product_WPF.Extensions;
 using C698_Product_WPF.Persistence.Repositories.Interfaces;
 
 namespace C698_Product_WPF
@@ -11,6 +15,8 @@ namespace C698_Product_WPF
   {
     private readonly IPartRepository _partRepository;
     private readonly IProductRepository _productRepository;
+    List<Part> parts;
+    List<Product> products;
 
     public MainWindow(IPartRepository partRepository, IProductRepository productRepository)
     {
@@ -22,13 +28,30 @@ namespace C698_Product_WPF
 
     private async Task GetData()
     {
-      PartsGrid.ItemsSource = await _partRepository.GetAll();
-      ProductsGrid.ItemsSource = await _productRepository.GetAll();
+      parts = await _partRepository.GetAll();
+      products = await _productRepository.GetAll();
+      PartsGrid.ItemsSource = parts;
+      ProductsGrid.ItemsSource = products;
     }
 
-    private void Exit_Click(object s, RoutedEventArgs e)
+    private void Parts_Search_Click(object s, RoutedEventArgs e)
     {
+      if (Parts_Search.Text.TrimFix() != null && Parts_Search.Text.TrimFix().Length > 0)
+      {
+        PartsGrid.ItemsSource = parts.Where(x => x.Name.ToLower().Contains(Parts_Search.Text.ToLower())).ToList();
+      }
+      else
+        PartsGrid.ItemsSource = parts;
+    }
 
+    private void Products_Search_Click(object s, RoutedEventArgs e)
+    {
+      if (Products_Search.Text.TrimFix() != null && Products_Search.Text.TrimFix().Length > 0)
+      {
+        ProductsGrid.ItemsSource = products.Where(x => x.Name.ToLower().Contains(Products_Search.Text.ToLower())).ToList();
+      }
+      else
+        ProductsGrid.ItemsSource = products;
     }
 
     private void Parts_Add_Click(object s, RoutedEventArgs e)
@@ -59,6 +82,11 @@ namespace C698_Product_WPF
     private void Products_Delete_Click(object s, RoutedEventArgs e)
     {
 
+    }
+
+    private void Exit_Click(object s, RoutedEventArgs e)
+    {
+      Application.Current.Shutdown();
     }
   }
 }
